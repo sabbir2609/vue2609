ae51274c13d488ea7ee2964a399e0eeda94912a3
sabbir2609 <sabbirhasan2999@gmail.com>
2020-08-06 21:21:23 +0600

    new app1 test

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
 var study = new Vue({
-  el: "#vue-app",
+  el: "#app",
   data: {
     heading: "Vue.js + Bulma",
     text: "The amount of sleep needed by the average person is ten minutes more."
   }
 });
+
+var hide = new Vue({
+  el:"#app1",
+  data: {
+    msg: true
+  }
+})
